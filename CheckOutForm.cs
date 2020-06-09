8fee0cfaa96eed72656a5cc76cd775b7e163425a
Angelina Dachkinova <a.dachkinova@gmail.com>
2020-06-09 21:29:13 +0300

    commit

--- CheckOutForm.cs
+++ CheckOutForm.cs
@@ -267,54 +267,58 @@ namespace HotelMenagementSystem
 
             string path = @"D:\SavedOrder-CafeManagemnetSystem\listOfOrderedProducts.txt";
 
-            if (!File.Exists(path))
-            {
-                Directory.CreateDirectory(@"D:\SavedOrder-CafeManagemnetSystem");
-                using (StreamWriter sw = File.CreateText(path))       
-                {
-                    sw.WriteLine("Ordered products list: ");
-                    sw.WriteLine();
-                    foreach (var product in productsList1)      
-                    {
-                        sw.WriteLine(product);   
-                    }
-                    sw.WriteLine();
-                    sw.WriteLine("Total bill: {0} ", totalBill1);
-                }
-                MessageBox.Show("Order successfully saved to file!");
 
-            }
-            else if (File.Exists(path))
-            {
-                String path_current = path;
-                int count = 0;
+            //FileSaver.CreateFile(path, productsList1);
 
-                while (File.Exists(path_current))
-                {
-                    count++;
-                    path_current = Path.GetDirectoryName(path)
-                                     + Path.DirectorySeparatorChar
-                                     + Path.GetFileNameWithoutExtension(path)
-                                     + "("
-                                     + count.ToString()
-                                     + ")"
-                                     + Path.GetExtension(path);
-                }
-                using (StreamWriter sw = new StreamWriter(path_current))
-                {
-                    sw.WriteLine("Ordered products list:");
-                    sw.WriteLine();
-                    foreach (var product in productsList1)
-                    {
-                        sw.WriteLine(product);
-                    }
-                    sw.WriteLine();
-                    sw.WriteLine("Total bill: {0}", totalBill1);
-                }
-                MessageBox.Show("Order successfully saved to file!");
-            }
+        //    if (!File.Exists(path))
+        //    {
+        //        Directory.CreateDirectory(@"D:\SavedOrder-CafeManagemnetSystem");
+        //        using (StreamWriter sw = File.CreateText(path))       
+        //        {
+        //            sw.WriteLine("Ordered products list: ");
+        //            sw.WriteLine();
+        //            foreach (var product in productsList1)      
+        //            {
+        //                sw.WriteLine(product);   
+        //            }
+        //            sw.WriteLine();
+        //            sw.WriteLine("Total bill: {0} ", totalBill1);
+        //        }
+        //        MessageBox.Show("Order successfully saved to file!");
+
+        //    }
+        //    else if (File.Exists(path))
+        //    {
+        //        String path_current = path;
+        //        int count = 0;
+
+        //        while (File.Exists(path_current))
+        //        {
+        //            count++;
+        //            path_current = Path.GetDirectoryName(path)
+        //                             + Path.DirectorySeparatorChar
+        //                             + Path.GetFileNameWithoutExtension(path)
+        //                             + "("
+        //                             + count.ToString()
+        //                             + ")"
+        //                             + Path.GetExtension(path);
+        //        }
+        //        using (StreamWriter sw = new StreamWriter(path_current))
+        //        {
+        //            sw.WriteLine("Ordered products list:");
+        //            sw.WriteLine();
+        //            foreach (var product in productsList1)
+        //            {
+        //                sw.WriteLine(product);
+        //            }
+        //            sw.WriteLine();
+        //            sw.WriteLine("Total bill: {0}", totalBill1);
+        //        }
+        //        MessageBox.Show("Order successfully saved to file!");
+        //    }
         }
     }
 }
+
 
     
